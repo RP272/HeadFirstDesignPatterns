c56df4f28ed6fc27f311e73094f59a0c1c9b8682
RP272 <272931@student.pwr.edu.pl>
2025-09-08 20:22:53 +0200

    feat: add HeatIndexDisplay and use it inside WeatherStation

--- src/SubjectObserverPattern/WeatherStation.java
+++ src/SubjectObserverPattern/WeatherStation.java
@@ -4,9 +4,13 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(12.34f, 35.6f, 1.45f);
         weatherData.setMeasurements(43.21f, 65.3f, 5.41f);
         weatherData.setMeasurements(12.21f, 35.53f, 1.1f);
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
     }
 }
