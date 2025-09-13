4808b33aa78a0c3fbe9d44b82edf767f3fec0185
RP272 <272931@student.pwr.edu.pl>
2025-09-13 12:27:56 +0200

    feat: add Size enum with getter/setter to Beverage class

--- src/BeverageDecorator/Beverage.java
+++ src/BeverageDecorator/Beverage.java
@@ -2,10 +2,19 @@ package BeverageDecorator;
 
 public abstract class Beverage {
     String description = "Unknown Beverage";
+    Size size;
 
     public String getDescription(){
         return description;
     }
 
     public abstract double cost();
+
+    public void setSize(Size newSize){
+        size = newSize;
+    }
+
+    public Size getSize(){
+        return size;
+    }
 }
