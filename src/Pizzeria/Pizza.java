492ee9d9606e5c8170d1d73588ae799a12ed54aa
RP272 <272931@student.pwr.edu.pl>
2025-09-15 20:39:41 +0200

    feat: original Pizzeria code

---
+++ src/Pizzeria/Pizza.java
@@ -0,0 +1,8 @@
+package Pizzeria;
+
+public interface Pizza {
+    void prepare();
+    void bake();
+    void cut();
+    void box();
+}
---
+++ src/Pizzeria/Pizzeria.java
@@ -0,0 +1,22 @@
+package Pizzeria;
+
+public class Pizzeria {
+    Pizza orderPizza(String type){
+        Pizza pizza = null;
+        if (type.equals("cheese")){
+            pizza = new CheesePizza();
+        } else if (type.equals("pepperoni")){
+            pizza = new PepperoniPizza();
+        } else if (type.equals("clam")){
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")){
+            pizza = new VeggiePizza();
+        }
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
