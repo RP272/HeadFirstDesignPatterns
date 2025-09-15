cf78d528c3d35b4ae4c0dd9e718e87c7b7ed0170
RP272 <272931@student.pwr.edu.pl>
2025-09-15 20:46:08 +0200

    refactor: use SimplePizzaFactory for Pizza object creation

--- src/Pizzeria/Pizzeria.java
+++ src/Pizzeria/Pizzeria.java
@@ -1,17 +1,16 @@
 package Pizzeria;
 
 public class Pizzeria {
-    Pizza orderPizza(String type){
-        Pizza pizza = null;
-        if (type.equals("cheese")){
-            pizza = new CheesePizza();
-        } else if (type.equals("pepperoni")){
-            pizza = new PepperoniPizza();
-        } else if (type.equals("clam")){
-            pizza = new ClamPizza();
-        } else if (type.equals("veggie")){
-            pizza = new VeggiePizza();
-        }
+    SimplePizzaFactory factory;
+
+    public Pizzeria(SimplePizzaFactory factory){
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+
+        pizza = factory.createPizza(type);
 
         pizza.prepare();
         pizza.bake();
