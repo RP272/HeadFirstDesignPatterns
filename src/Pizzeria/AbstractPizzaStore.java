2dfe39ebbf5ecd8b77967c5bf1e8953096eac7ae
RP272 <272931@student.pwr.edu.pl>
2025-09-16 20:08:30 +0200

    refactor: AbstractPizzaStore

---
+++ src/Pizzeria/AbstractPizzaStore.java
@@ -0,0 +1,18 @@
+package Pizzeria;
+
+public abstract class AbstractPizzaStore {
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+
+        pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    public abstract Pizza createPizza(String type);
+}
---
+++ src/Pizzeria/NYStylePizzaStore.java
@@ -0,0 +1,8 @@
+package Pizzeria;
+
+public class NYStylePizzaStore extends AbstractPizzaStore{
+    @Override
+    public Pizza createPizza(String type) {
+        return null;
+    }
+}
